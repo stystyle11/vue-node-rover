af3687efc25498a0a3855793d11adaecbb095695
Estevao Adam <estevao.rafael@cactusgaming.net>
2024-09-04 05:51:30 -0300

    adding validation to routes

--- backend/src/Routes/roverRoutes.js
+++ backend/src/Routes/roverRoutes.js
@@ -1,12 +1,49 @@
 import { Router } from 'express';
 import RoverController from '../Controllers/RoverController.js';
+import { body, param } from 'express-validator';
 
 const router = Router();
 
-router.post('/rovers', RoverController.createRover);
+/**
+ * Post and Put Rovers
+ */ 
+
+router.post('/rovers',
+    [
+        body('name').isString().notEmpty(),
+        body('color').isString().notEmpty(),
+    ],
+    
+    RoverController.createRover);
+
+router.put('/rovers/:id',  
+    [
+            param('id').isInt(),
+            body('name').isString().optional(),
+            body('color').isString().optional(),
+    ],
+    
+    RoverController.updateRover);   
+
+/**
+ * GET Rovers
+ */ 
+
 router.get('/rovers', RoverController.getAllRovers);
-router.get('/rovers/:id', RoverController.getRoverById);
-router.put('/rovers/:id', RoverController.updateRover);
-router.delete('/rovers/:id', RoverController.deleteRover);
+
+router.get('/rovers/:id',
+    [param('id').isInt()],
+    
+    RoverController.getRoverById);
+
+/**
+ * Delete Rovers
+ */ 
+
+router.delete('/rovers/:id', 
+    [param('id').isInt()],
+    
+    RoverController.deleteRover);
+
 
 export default router;
