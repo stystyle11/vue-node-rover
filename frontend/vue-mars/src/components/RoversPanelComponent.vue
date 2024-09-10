8953eb47b32997acb30ed741ed0647ef34aff541
Estevao Adam <estevao.rafael@cactusgaming.net>
2024-09-10 06:42:01 -0300

    logic done, waiting on refactoring and tests

--- frontend/vue-mars/src/components/RoversPanelComponent.vue
+++ frontend/vue-mars/src/components/RoversPanelComponent.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>Rover Panel:{{ name }}</h1>
-    <h5>Landing Position:{{ roverInstructionsModel }}</h5>
 
+    <h5>Rover instructionModel:{{ roverInstructionsModel }}</h5>
+    <h5>Landing Position:{{ landingPositionModel }}</h5>
     <input
       v-model="landingPositionModel"
       @input="handleInput"
@@ -23,6 +24,7 @@
       @blur="handleBlur"
       @focus="handleFocus"
     />
+    Direction:{{ heading }}
     <!-- 
     <InputBaseComponent
       :modelValueFromParent="roverInstructionsModel"
@@ -40,10 +42,10 @@
 </template>
 
 <script setup>
-import { ref, defineProps } from 'vue';
+import { ref, computed } from 'vue';
 //import InputBaseComponent from '@/components/InputBaseComponent.vue';
 import ButtonBaseComponent from '@/components/ButtonBaseComponent.vue';
-//provide('landingPosition');
+
 const props = defineProps({
   name: {
     type: String,
@@ -62,11 +64,117 @@ const props = defineProps({
 const maxlength = 15;
 const landingPositionModel = ref(props.landingPosition);
 const roverInstructionsModel = ref(props.instructionsPosition);
+const finalPosition = ref('');
+const movingPosition = ref('');
 
-const headingToNumber = () => {};
-const calculateHeading = () => {};
-const calculatePositonForward = () => {};
-const moveRovers = (instructionsArray) => {};
+// Computed HEading
+const computedHeading = computed(() => {
+  if (finalPosition.value.length > 0) {
+    return finalPosition.value[2];
+  } else {
+    return landingPositionModel.value[2];
+  }
+});
+// Get the Heading of the Rover
+const heading = computedHeading.value;
+
+const headingToNumber = (headingString) => {
+  switch (headingString) {
+    case 'N':
+      return 0;
+    case 'E':
+      return 90;
+    case 'S':
+      return 180;
+    case 'W':
+      return 270;
+    default:
+      return 0;
+  }
+};
+const headingNumber = headingToNumber(heading);
+
+const calculateHeading = (inputString) => {
+  if (inputString === 'L') {
+    headingNumber.value = headingNumber - 90;
+  } else {
+    headingNumber.value = headingNumber + 90;
+  }
+  headingNumber.value > 270
+    ? 0
+    : headingNumber.value < 0
+      ? 270
+      : headingNumber.value;
+
+  switch (headingNumber.value) {
+    case 0:
+      return 'N';
+    case 90:
+      return 'E';
+    case 180:
+      return 'S';
+    case 270:
+      return 'W';
+    default:
+      return 'N';
+  }
+};
+const calculateMovingForward = (headingString) => {
+  switch (headingString.value) {
+    case 'N':
+      return ['x', 'y1', 'N'];
+    case 'E':
+      return ['x1', 'y', 'E'];
+    case 'S':
+      return ['x', 'y-1', 'S'];
+    case 'W':
+      return ['x-1', 'y', 'W'];
+    default:
+      return ['x', 'y', 'N'];
+  }
+};
+const updatePositions = (oldPosition, newPosition) => {
+  let xSum =
+    parseInt(oldPosition[0].replace('x', '')) +
+    parseInt(newPosition[0].replace('x', ''));
+  let ySum =
+    parseInt(oldPosition[1].replace('y', '')) +
+    parseInt(newPosition[1].replace('y', ''));
+
+  return [`x${xSum}`, `y${ySum}`, newPosition[2]];
+};
+const createArrayFromInstructionString = (instructionsString, stringSize) => {
+  const instructionsArray = [];
+
+  for (let i = 0; i < instructionsString.length; i += stringSize) {
+    instructionsArray.push(instructionsString.substring(i, i + stringSize));
+  }
+
+  return instructionsArray;
+};
+const finalArrayInstructions = createArrayFromInstructionString(
+  roverInstructionsModel,
+);
+const getFinalPosition = (instructionsStringArray) => {
+  instructionsStringArray.forEach((string) => {
+    let currentHeading;
+    let currentPosition;
+
+    if (string === 'L' || string === 'M') {
+      currentHeading = calculateHeading(string);
+    } else {
+      currentPosition = calculateMovingForward(currentHeading);
+    }
+    movingPosition.value = updatePositions(
+      movingPosition.value,
+      currentPosition,
+    );
+  });
+  finalPosition.value = updatePositions(
+    landingPositionModel.value,
+    movingPosition.value,
+  );
+};
 
 const onBlur = () => {
   console.log('Input blurred');
